f3013508aebeced70a72226b61df5077fa8b7016
binyamyohannes <olangobinyam@gmail.com>
2023-12-25 14:50:19 +0300

    Add cart table

--- src/Cart.js
+++ src/Cart.js
@@ -1,7 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+
 const Cart = () => {
   return (
     <div className="cart-page">
-      <h1>Hello, this is my cart.</h1>
+      <Container>
+        <h1>Hello, this is my cart.</h1>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product Name</th>
+              <th>Price</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Container>
     </div>
   );
 }
