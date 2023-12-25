6608a3ddf1fde606002b4e57b5aed2afa436e173
binyamyohannes <olangobinyam@gmail.com>
2023-12-25 21:46:42 +0300

    Fix category display

--- src/Category.js
+++ src/Category.js
@@ -1,20 +1,28 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 
 const Category = (props) => {
-  const { products } = props;
+  const {
+    handleAllProducts,
+    handleElectronicsProducts,
+    handleJeweleryProducts,
+    handleMenClothingProducts,
+    handleWomenClothingProducts,
+    category
+  } = props;
 
   return (
     <div className="category__dropdown">
       <Dropdown>
         <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-          Categories
+          {category}
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1">Electronics</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Jewelry</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Men's clothing</Dropdown.Item>
-          <Dropdown.Item href="#/action-4">Women's clothing</Dropdown.Item>
+          <Dropdown.Item onClick={handleAllProducts}>All Products</Dropdown.Item>
+          <Dropdown.Item onClick={handleElectronicsProducts}>Electronics</Dropdown.Item>
+          <Dropdown.Item onClick={handleJeweleryProducts}>Jewelry</Dropdown.Item>
+          <Dropdown.Item onClick={handleMenClothingProducts}>Men's clothing</Dropdown.Item>
+          <Dropdown.Item onClick={handleWomenClothingProducts}>Women's clothing</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     </div>
--- src/Product.js
+++ src/Product.js
@@ -5,20 +5,75 @@ import { useEffect, useState } from 'react';
 
 const Product = () => {
   const [products, setProducts] = useState(null);
+  const [category, setCategory] = useState('')
+
+  const url = 'https://fakestoreapi.com/products';
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
+    fetch(url)
     .then(res=>res.json())
     .then(data=>{
       setProducts(data);
-      console.log(data);
     });
+    setCategory("Select Category")
   }, []);
+
+  const handleAllProducts = () => {
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+      setProducts(data);
+    });
+    setCategory('All Products')
+  };
+
+  const handleElectronicsProducts = () => {
+    fetch(`${url}/category/electronics`)
+    .then(res=>res.json())
+    .then(data=>{
+      setProducts(data);
+    });
+    setCategory('Electronics')
+  };
+
+  const handleJeweleryProducts = () => {
+    fetch(`${url}/category/jewelery`)
+    .then(res=>res.json())
+    .then(data=>{
+      setProducts(data);
+    });
+    setCategory('Jewelry')
+  };
+
+  const handleMenClothingProducts = () => {
+    fetch(`${url}/category/men's clothing`)
+    .then(res=>res.json())
+    .then(data=>{
+      setProducts(data);
+    });
+    setCategory("Men's Clothing")
+  };
+
+  const handleWomenClothingProducts = () => {
+    fetch(`${url}/category/women's clothing`)
+    .then(res=>res.json())
+    .then(data=>{
+      setProducts(data);
+    });
+    setCategory("Women's Clothing")
+  };
 
   return (
     <div className="product-page">
       <Container>
-        <Category products={products} />
+        <Category
+          handleAllProducts={handleAllProducts}
+          handleElectronicsProducts={handleElectronicsProducts}
+          handleJeweleryProducts={handleJeweleryProducts}
+          handleMenClothingProducts={handleMenClothingProducts}
+          handleWomenClothingProducts={handleWomenClothingProducts}
+          category={category}
+        />
         <ProductList products={products} />
       </Container>
     </div>
