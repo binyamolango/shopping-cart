e6a421305e259704f07aedfb986b50b2bd306552
binyamyohannes <olangobinyam@gmail.com>
2023-12-31 16:13:58 +0300

    Add 3D box

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cart from "./components/Cart";
 import Header from "./components/Header";
 import Product from "./components/Product";
+import Box from "./components/Box";
+import { Canvas } from "react-three-fiber";
 
 function App() {
   return (
@@ -9,6 +11,12 @@ function App() {
       <div className="App">
           <Header />
         <div className="content">
+          <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[1.2, 0, 0]} />
+          </Canvas>
           <Routes>
             <Route path="/" element={<Product />} />
             <Route path="/cart" element={<Cart />} />
